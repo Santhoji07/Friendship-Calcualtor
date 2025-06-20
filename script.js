e67208201c03601e7b037892fa538d51224bab0e
Santhoji07 <santhoji07msd@gmail.com>
2025-06-20 11:25:14 +0530

    cal

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function cal(){
     var fname = document.getElementById("name2").value;
 
     var result=Math.random()*100;
-
+    var par = document.getElementById("result");
     par.innerHTML = "The compatibility between " + mname + " and " + fname + " is " + result.toFixed(2) + "%";
 
+    if (result > 80) {
+        par.style.border.color = "green";
+    } else if (result > 60 && result <= 80) {
+        par.style.color = "orange";
+    } else{
+        par.style.color = "red";
+    }
 }
